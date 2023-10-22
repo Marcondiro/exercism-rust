9a9e27ade26082e726ff53df711bad6470abac68
Marcondiro <46560192+Marcondiro@users.noreply.github.com>
2023-10-22 22:29:50 +0200

    feat(knapsack): add solution

--- exercises/practice/knapsack/src/lib.rs
+++ exercises/practice/knapsack/src/lib.rs
@@ -1,9 +1,24 @@
-#[derive(Debug)]
 pub struct Item {
     pub weight: u32,
     pub value: u32,
 }
 
 pub fn maximum_value(max_weight: u32, items: &[Item]) -> u32 {
-    todo!("calculate the maximum value achievable with the given {items:?} and {max_weight}");
+    let mut knapsack_previous = vec![0u32; max_weight as usize + 1];
+    let mut knapsack = vec![0u32; max_weight as usize + 1];
+
+    for item in items {
+        for knapsack_weight in 1usize..=(max_weight as usize) {
+            if item.weight as usize <= knapsack_weight
+                && knapsack_previous[knapsack_weight]
+                    <= knapsack_previous[knapsack_weight - item.weight as usize] + item.value
+            {
+                knapsack[knapsack_weight] =
+                    knapsack_previous[knapsack_weight - item.weight as usize] + item.value;
+            }
+        }
+        knapsack_previous = knapsack.clone();
+    }
+
+    knapsack[max_weight as usize]
 }
