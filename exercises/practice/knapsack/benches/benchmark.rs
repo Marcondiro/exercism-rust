141fc5c0693cbb36ba4ab1850d9723a92cf1e056
Marcondiro <46560192+Marcondiro@users.noreply.github.com>
2023-10-23 19:12:42 +0200

    feat(knapsack): add benchmak

--- exercises/practice/knapsack/benches/benchmark.rs
+++ exercises/practice/knapsack/benches/benchmark.rs
@@ -3,11 +3,40 @@ extern crate knapsack;
 extern crate test;
 
 use knapsack::Item;
+use std::cmp;
+use std::hint::black_box;
 use test::Bencher;
 
 #[bench]
-fn bench(b: &mut Bencher) {
-    b.iter(|| knapsack::maximum_value(MAX_WEIGHT, &ITEMS));
+fn bench_fifteen(b: &mut Bencher) {
+    b.iter(||
+        knapsack::maximum_value(MAX_WEIGHT, black_box(&ITEMS))
+    );
+}
+#[bench]
+fn bench_fifteen_brute_force(b: &mut Bencher) {
+    b.iter(||
+        maximum_value(MAX_WEIGHT, black_box(&ITEMS))
+    );
+}
+
+/// Simple and inefficient brute force. Can it be beat?
+pub fn maximum_value(max_weight: u32, items: &[Item]) -> u32 {
+    if items.len() == 0 {
+        return 0;
+    }
+
+    // Put the item in the knapsack (if fits)
+    let mut value_taking = 0;
+    if items[0].weight <= max_weight {
+        value_taking = items[0].value + maximum_value(max_weight - items[0].weight, &items[1..]);
+    }
+
+    // Don't put the item in the knapsack
+    let value_not_taking = maximum_value(max_weight, &items[1..]);
+
+    // Return the value of the richest knapsack
+    cmp::max(value_taking, value_not_taking)
 }
 
 const MAX_WEIGHT: u32 = 750;
