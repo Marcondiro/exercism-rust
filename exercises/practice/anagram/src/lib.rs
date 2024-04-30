a71519e695e96806b6c3f11b30c6dd0c2e49b28a
Marcondiro <cavenati.marco@gmail.com>
2024-04-30 15:39:24 +0200

    Add anagram and armstrong-numbers solution

--- exercises/practice/anagram/src/lib.rs
+++ exercises/practice/anagram/src/lib.rs
@@ -1,5 +1,25 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    // todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+    let mut anagrams = HashSet::new();
+    'possible_anagrams: for &p in possible_anagrams {
+        let p_lower = p.to_lowercase();
+        let mut available_chars = word.to_lowercase();
+        if p_lower.len() != word.len() || p_lower == available_chars {
+            continue 'possible_anagrams;
+        }
+
+        for c in p_lower.chars() {
+            let i = available_chars.find(&c.to_string());
+            match i {
+                None => continue 'possible_anagrams,
+                Some(i) => available_chars.remove(i),
+            };
+        }
+
+        anagrams.insert(p);
+    }
+
+    anagrams
 }
--- exercises/practice/armstrong-numbers/src/lib.rs
+++ exercises/practice/armstrong-numbers/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    todo!("true if {num} is an armstrong number")
+    // todo!("true if {num} is an armstrong number")
+    let len = num.to_string().len() as u32;
+    let mut sum: u32 = 0;
+
+    for d in num.to_string().chars().map(|c| c.to_digit(10).unwrap()) {
+        sum = match d.checked_pow(len).and_then(|v| v.checked_add(sum)) {
+            None => return false,
+            Some(v) => v,
+        }
+    }
+
+    num == sum
 }
